4854ea73a331716a9ffa232b1fb01eb91b266c17
Deivydas <deivydas.voroneckis@cern.ch>
2016-07-18 12:17:47 +0200

    Everything outputed in two ways.

--- DQMServices/Components/plugins/DQMStreamStats.cc
+++ DQMServices/Components/plugins/DQMStreamStats.cc
@@ -14,36 +14,36 @@ DQMStreamStats::DQMStreamStats(edm::ParameterSet const &) {}
 DQMStreamStats::~DQMStreamStats() {}
 
 //Set all possible x dimension parameters
-void DQMStreamStats::getDimensionX(HistoEntry &e, MonitorElement *m){
-  e.nXBin = m->getNbinsX();
-  e.xLow = m->getTH1()->GetXaxis()->GetXmin();
-  e.xUp = m->getTH1()->GetXaxis()->GetXmax();
-  e.xMean = m->getTH1()->GetMean();
-  e.xMeanError = m->getTH1()->GetMeanError();
-  e.xRms = m->getTH1()->GetRMS();
-  e.xRmsError = m->getTH1()->GetRMSError();
-  e.xUnderflow = m->getTH1()->GetBinContent(0);
-  e.xOverflow = m->getTH1()->GetBinContent(e.nXBin + 1);
+void DQMStreamStats::getDimensionX(Dimension &d, MonitorElement *m){
+  d.nBin = m->getNbinsX();
+  d.low = m->getTH1()->GetXaxis()->GetXmin();
+  d.up = m->getTH1()->GetXaxis()->GetXmax();
+  d.mean = m->getTH1()->GetMean();
+  d.meanError = m->getTH1()->GetMeanError();
+  d.rms = m->getTH1()->GetRMS();
+  d.rmsError = m->getTH1()->GetRMSError();
+  d.underflow = m->getTH1()->GetBinContent(0);
+  d.overflow = m->getTH1()->GetBinContent(d.nBin + 1);
 }
 
-void DQMStreamStats::getDimensionY(HistoEntry &e, MonitorElement *m){
-  e.nYBin = m->getNbinsY();
-  e.yLow = m->getTH1()->GetYaxis()->GetXmin();
-  e.yUp = m->getTH1()->GetYaxis()->GetXmax();
-  e.yMean = m->getTH1()->GetMean(2);
-  e.yMeanError = m->getTH1()->GetMeanError(2);
-  e.yRms = m->getTH1()->GetRMS(2);
-  e.yRmsError = m->getTH1()->GetRMSError(2);
+void DQMStreamStats::getDimensionY(Dimension &d, MonitorElement *m){
+  d.nBin = m->getNbinsY();
+  d.low = m->getTH1()->GetYaxis()->GetXmin();
+  d.up = m->getTH1()->GetYaxis()->GetXmax();
+  d.mean = m->getTH1()->GetMean(2);
+  d.meanError = m->getTH1()->GetMeanError(2);
+  d.rms = m->getTH1()->GetRMS(2);
+  d.rmsError = m->getTH1()->GetRMSError(2);
 }
 
-void DQMStreamStats::getDimensionZ(HistoEntry &e, MonitorElement *m){
-  e.nZBin = m->getNbinsZ();
-  e.zLow = m->getTH1()->GetZaxis()->GetXmin();
-  e.zUp = m->getTH1()->GetZaxis()->GetXmax();
-  e.zMean = m->getTH1()->GetMean(3);
-  e.zMeanError = m->getTH1()->GetMeanError(3);
-  e.zRms = m->getTH1()->GetRMS(3);
-  e.zRmsError = m->getTH1()->GetRMSError(3);
+void DQMStreamStats::getDimensionZ(Dimension &d, MonitorElement *m){
+  d.nBin = m->getNbinsZ();
+  d.low = m->getTH1()->GetZaxis()->GetXmin();
+  d.up = m->getTH1()->GetZaxis()->GetXmax();
+  d.mean = m->getTH1()->GetMean(3);
+  d.meanError = m->getTH1()->GetMeanError(3);
+  d.rms = m->getTH1()->GetRMS(3);
+  d.rmsError = m->getTH1()->GetRMSError(3);
 }
 
 HistoEntry DQMStreamStats::analyze(MonitorElement *m) {
@@ -51,16 +51,6 @@ HistoEntry DQMStreamStats::analyze(MonitorElement *m) {
   e.name = m->getName();
   e.path = m->getFullname();
   e.type = "unknown";
-  e.bin_count = 0;
-  e.bin_size = 0;
-  e.extra = 0;
-  e.total = 0;
- // e.nXBin = 0; e.xLow = 0; e.xUp = 0;
- // e.nYBin = 0; e.yLow = 0; e.yUp = 0;
- // e.nZBin = 0; e.zLow = 0; e.zUp = 0;
- // e.xMean = 0; e.xMeanError = 0; e.xRms = 0; e.xRmsError = 0; e.xUnderflow = 0; e.xOverflow = 0;
- // e.yMean = 0; e.yMeanError = 0; e.yRms = 0; e.yRmsError = 0;
- // e.zMean = 0; e.zMeanError = 0; e.zRms = 0; e.zRmsError = 0;
 
   switch (m->kind()) {
     case MonitorElement::DQM_KIND_INVALID:
@@ -84,57 +74,57 @@ HistoEntry DQMStreamStats::analyze(MonitorElement *m) {
     case MonitorElement::DQM_KIND_TH1F:
       e.type = "TH1F";
       e.bin_size = sizeof(float);
-      getDimensionX(e, m);
+      getDimensionX(e.dimX, m);
       break;
     case MonitorElement::DQM_KIND_TH1S:
       e.type = "TH1S";
       e.bin_size = sizeof(short);
-      getDimensionX(e, m);
+      getDimensionX(e.dimX, m);
       break;
     case MonitorElement::DQM_KIND_TH1D:
       e.type = "TH1D";
       e.bin_size = sizeof(double);
-      getDimensionX(e, m);
+      getDimensionX(e.dimX, m);
       break;
     case MonitorElement::DQM_KIND_TPROFILE:
       e.type = "TProfile";
       e.bin_size = sizeof(double);
-      getDimensionX(e, m);
+      getDimensionX(e.dimX, m);
       break;
 
     // two-dim ME
     case MonitorElement::DQM_KIND_TH2F:
       e.type = "TH2F";
       e.bin_size = sizeof(float);
-      getDimensionX(e, m);
-      getDimensionY(e, m);
+      getDimensionX(e.dimX, m);
+      getDimensionY(e.dimY, m);
       break;
     case MonitorElement::DQM_KIND_TH2S:
       e.type = "TH2S";
       e.bin_size = sizeof(short);
-      getDimensionX(e, m);
-      getDimensionY(e, m);
+      getDimensionX(e.dimX, m);
+      getDimensionY(e.dimY, m);
       break;
     case MonitorElement::DQM_KIND_TH2D:
       e.type = "TH2D";
       e.bin_size = sizeof(double);
-      getDimensionX(e, m);
-      getDimensionY(e, m);
+      getDimensionX(e.dimX, m);
+      getDimensionY(e.dimY, m);
       break;
     case MonitorElement::DQM_KIND_TPROFILE2D:
       e.type = "TProfile2D";
       e.bin_size = sizeof(double);
-      getDimensionX(e, m);
-      getDimensionY(e, m);
+      getDimensionX(e.dimX, m);
+      getDimensionY(e.dimY, m);
       break;
 
     // three-dim ME
     case MonitorElement::DQM_KIND_TH3F:
       e.type = "TH3F";
       e.bin_size = sizeof(float);
-      getDimensionX(e, m);
-      getDimensionY(e, m);
-      getDimensionZ(e, m);
+      getDimensionX(e.dimX, m);
+      getDimensionY(e.dimY, m);
+      getDimensionZ(e.dimZ, m);
       break;
 
     default:
@@ -145,6 +135,7 @@ HistoEntry DQMStreamStats::analyze(MonitorElement *m) {
   // skip "INVALID", "INT", "REAL", "STRING", "unknown"
   if (!strcmp(e.type,"INT") && !strcmp(e.type,"STRING") && !strcmp(e.type,"REAL") && !strcmp(e.type,"unknown")) {
       e.bin_count = m->getTH1()->GetNcells();
+      e.entries = m->getEntries();
   }
 
   e.total = e.bin_count * e.bin_size + e.extra;
@@ -186,7 +177,7 @@ void DQMStreamStats::group(HistoStats& stats) {
 }
 #endif
 
-void DQMStreamStats::writeJson(const std::string &fn, const HistoStats &stats) {
+void DQMStreamStats::writeMemoryJson(const std::string &fn, const HistoStats &stats) {
   using boost::property_tree::ptree;
 
   ptree doc;
@@ -204,11 +195,11 @@ void DQMStreamStats::writeJson(const std::string &fn, const HistoStats &stats) {
   ptree bin_sizes;
   ptree extras;
   ptree totals;
+  ptree entries;
 
   for (auto &stat : stats) {
     
     ptree infoChild;
-
     infoChild.put("", stat.path);
     paths.push_back(std::make_pair("", infoChild));
     infoChild.put("", stat.type);
@@ -221,6 +212,8 @@ void DQMStreamStats::writeJson(const std::string &fn, const HistoStats &stats) {
     extras.push_back(std::make_pair("", infoChild));
     infoChild.put("", stat.total);
     totals.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.entries);
+    entries.push_back(std::make_pair("", infoChild));
 
     ptree child;
     child.put("path", stat.path);
@@ -229,6 +222,7 @@ void DQMStreamStats::writeJson(const std::string &fn, const HistoStats &stats) {
     child.put("bin_size", stat.bin_size);
     child.put("extra", stat.extra);
     child.put("total", stat.total);
+    child.put("entries", stat.entries);
 
     histograms.push_back(std::make_pair("", child));
   }
@@ -239,6 +233,165 @@ void DQMStreamStats::writeJson(const std::string &fn, const HistoStats &stats) {
   info.add_child("bin_size", bin_sizes);
   info.add_child("extra", extras);
   info.add_child("total", totals);
+  info.add_child("entries", entries);
+  histograms.push_front(std::make_pair("", info));
+
+  doc.add_child("histograms", histograms);
+
+  std::ofstream file(fn);
+  write_json(file, doc, true);
+  file.close();
+};
+
+void DQMStreamStats::writeHistogramJson(const std::string &fn, const HistoStats &stats) {
+  using boost::property_tree::ptree;
+
+  ptree doc;
+
+  doc.put("pid", ::getpid());
+  doc.put("n_entries", stats.size());
+  doc.put("update_timestamp", std::time(NULL));
+
+  ptree histograms;
+
+  ptree info;
+  ptree paths;
+  ptree bin_counts;
+  ptree entries;
+  ptree names;
+  ptree xNBins, yNBins, zNBins;
+  ptree xLows, yLows, zLows;
+  ptree xUps, yUps, zUps;
+  ptree xMeans, yMeans, zMeans;
+  ptree xMeanErrors, yMeanErrors, zMeanErrors;
+  ptree xRms, yRms, zRms;
+  ptree xRmsErrors, yRmsErrors, zRmsErrors;
+  ptree xUnderflows, xOverflows;
+
+  for (auto &stat : stats) {
+    ptree infoChild;
+    infoChild.put("", stat.path);
+    paths.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.bin_count);
+    bin_counts.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.entries);
+    entries.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.name);
+    names.push_back(std::make_pair("", infoChild));
+
+    infoChild.put("", stat.dimX.nBin);
+    xNBins.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.dimX.low);
+    xLows.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.dimX.up);
+    xUps.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.dimY.nBin);
+    yNBins.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.dimY.low);
+    yLows.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.dimY.up);
+    yUps.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.dimZ.nBin);
+    zNBins.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.dimZ.low);
+    zLows.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.dimZ.up);
+    zUps.push_back(std::make_pair("", infoChild));
+    
+    infoChild.put("", stat.dimX.mean);
+    xMeans.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.dimX.meanError);
+    xMeanErrors.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.dimX.rms);
+    xRms.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.dimX.rmsError);
+    xRmsErrors.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.dimX.underflow);
+    xUnderflows.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.dimX.overflow);
+    xOverflows.push_back(std::make_pair("", infoChild));
+
+    infoChild.put("", stat.dimY.mean);
+    yMeans.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.dimY.meanError);
+    yMeanErrors.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.dimY.rms);
+    yRms.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.dimY.rmsError);
+    yRmsErrors.push_back(std::make_pair("", infoChild));
+
+    infoChild.put("", stat.dimZ.mean);
+    zMeans.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.dimZ.meanError);
+    zMeanErrors.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.dimZ.rms);
+    zRms.push_back(std::make_pair("", infoChild));
+    infoChild.put("", stat.dimZ.rmsError);
+    zRmsErrors.push_back(std::make_pair("", infoChild));
+
+    ptree child;
+    child.put("path", stat.path);
+    child.put("bin_count", stat.bin_count);
+    child.put("entries", stat.entries);
+    child.put("name", stat.name);
+
+    child.put("x_bins", stat.dimX.nBin);
+    child.put("x_low", stat.dimX.low);
+    child.put("x_up", stat.dimX.up);
+    child.put("y_bins", stat.dimY.nBin);
+    child.put("y_low", stat.dimY.low);
+    child.put("y_up", stat.dimY.up);
+    child.put("z_bins", stat.dimZ.nBin);
+    child.put("z_low", stat.dimZ.low);
+    child.put("z_up", stat.dimZ.up);
+    
+    child.put("x_mean", stat.dimX.mean);
+    child.put("x_mean_error", stat.dimX.meanError);
+    child.put("x_rms", stat.dimX.rms);
+    child.put("x_rms_error", stat.dimX.rmsError);
+    child.put("x_underflow", stat.dimX.underflow);
+    child.put("x_overflow", stat.dimX.overflow);
+
+    child.put("y_mean", stat.dimY.mean);
+    child.put("y_mean_error", stat.dimY.meanError);
+    child.put("y_rms", stat.dimY.rms);
+    child.put("y_rms_error", stat.dimY.rmsError);
+
+    child.put("z_mean", stat.dimZ.mean);
+    child.put("z_mean_error", stat.dimZ.meanError);
+    child.put("z_rms", stat.dimZ.rms);
+    child.put("z_rms_error", stat.dimZ.rmsError);
+
+    histograms.push_back(std::make_pair("", child));
+  }
+
+  info.add_child("path", paths);
+  info.add_child("bin_count", bin_counts);
+  info.add_child("entries", entries);
+  info.add_child("name", names);
+  info.add_child("x_bins", xNBins);
+  info.add_child("x_low", xLows);
+  info.add_child("x_up", xUps);
+  info.add_child("y_bins", yNBins);
+  info.add_child("y_low", yLows);
+  info.add_child("y_up", yUps);
+  info.add_child("z_bins", zNBins);
+  info.add_child("z_low", zLows);
+  info.add_child("z_up", zUps);
+  info.add_child("x_mean", xMeans);
+  info.add_child("x_mean_error", xMeanErrors);
+  info.add_child("x_rms", xRms);
+  info.add_child("x_rms_error", xRmsErrors);
+  info.add_child("x_underflow", xUnderflows);
+  info.add_child("x_overflow", xOverflows);
+  info.add_child("y_mean", yMeans);
+  info.add_child("y_mean_error", yMeanErrors);
+  info.add_child("y_rms", yRms);
+  info.add_child("y_rms_error", yRmsErrors);
+  info.add_child("z_mean", zMeans);
+  info.add_child("z_mean_error", zMeanErrors);
+  info.add_child("z_rms", zRms);
+  info.add_child("z_rms_error", zRmsErrors);
   histograms.push_front(std::make_pair("", info));
 
   doc.add_child("histograms", histograms);
@@ -253,7 +406,8 @@ void DQMStreamStats::dqmEndLuminosityBlock(DQMStore::IBooker &,
                                            edm::LuminosityBlock const &,
                                            edm::EventSetup const &) {
   HistoStats st = collect(iGetter);
-  //writeJson("outputJson", st);
+  writeMemoryJson("outputJson", st);
+  writeHistogramJson("outputHistogramJson", st);
 }
 
 #if 0
--- DQMServices/Components/plugins/DQMStreamStats.h
+++ DQMServices/Components/plugins/DQMStreamStats.h
@@ -25,28 +25,37 @@
 
 namespace dqmservices {
 
+struct Dimension{
+  int nBin = 0;
+  double low = 0, up = 0;
+  double mean = 0, meanError = 0; 
+  double rms = 0, rmsError = 0; 
+  double underflow = 0, overflow = 0;  
+};
+
 class HistoEntry {
  public:
   std::string path;
 
   std::string name;
   const char *type;
-  size_t bin_count;
-  size_t bin_size;
-  size_t extra;
-  size_t total;
-  int nXBin = 0;
-  double xLow = 0;
-  double xUp = 0;
+  size_t bin_count = 0;
+  size_t bin_size = 0;
+  size_t extra = 0;
+  size_t total = 0;
+  double entries = 0;
+  int maxBin = 0, minBin = 0;
+  double maxValue = 0, minValue = 0;
+  /*int nXBin = 0;
+  double xLow = 0, xUp = 0;
   int nYBin = 0;
-  double yLow = 0;
-  double yUp = 0;
+  double yLow = 0, yUp = 0;
   int nZBin = 0;
-  double zLow = 0;
-  double zUp = 0;
+  double zLow = 0, zUp = 0;
   double xMean = 0, xMeanError = 0, xRms = 0, xRmsError = 0, xUnderflow = 0, xOverflow = 0;
   double yMean = 0, yMeanError = 0, yRms = 0, yRmsError = 0;
-  double zMean = 0, zMeanError = 0, zRms = 0, zRmsError = 0;
+  double zMean = 0, zMeanError = 0, zRms = 0, zRmsError = 0; */
+  Dimension dimX, dimY, dimZ; 
 
   bool operator<(const HistoEntry &rhs) const { return path < rhs.path; }
 };
@@ -77,12 +86,13 @@ class DQMStreamStats : public DQMEDHarvester {
 
  protected:
   HistoStats collect(DQMStore::IGetter &iGetter);
-  void writeJson(const std::string &fn, const HistoStats &stats);
+  void writeMemoryJson(const std::string &fn, const HistoStats &stats);
+  void writeHistogramJson(const std::string &fn, const HistoStats &stats);
 
  private:
-  void getDimensionX(HistoEntry &e, MonitorElement *m);
-  void getDimensionY(HistoEntry &e, MonitorElement *m);
-  void getDimensionZ(HistoEntry &e, MonitorElement *m);
+  void getDimensionX(Dimension &d, MonitorElement *m);
+  void getDimensionY(Dimension &d, MonitorElement *m);
+  void getDimensionZ(Dimension &d, MonitorElement *m);
 };
 
 }  // end of namespace
